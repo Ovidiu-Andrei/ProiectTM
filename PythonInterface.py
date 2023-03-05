e6e4d89760e16cb5f5c0f8c58c0cab190f50d269
Ovidiu Andrei <ovidiuandrei1999@gmail.com>
2023-03-05 22:54:46 +0200

    Implement a resize button

--- PythonInterface.py
+++ PythonInterface.py
@@ -24,7 +24,9 @@ FirstWindow.iconbitmap(r'C:\Users\Ovidiu\Desktop\ProiectTM\ImageProcessing.ico')
 global img_o
 global pathToImage
 global SaveImage
-
+SaveImage = None
+global ResizeImage
+ResizeImage = None
 def open_file():
   global img_o
   global pathToImage
@@ -59,39 +61,135 @@ def warm(image):
 
 def save():
     pass
-   
+
+def grayFilter():
+  pass   
+
+def rgbFilter(image):
+	(B, G, R) = cv2.split(image) #spliting the img into RGB components
+	# find the maximum pixel intensity values for each
+	# (x, y)-coordinate,, then set all pixel values less
+	# than M to zero
+	M = np.maximum(np.maximum(R, G), B)
+	R[R < M] = 0
+	G[G < M] = 0
+	B[B < M] = 0
+	# merge the channels back together and return the image
+	return cv2.merge([B, G, R])
+
+def resize_func():
+    global width,height,disp_img,ResizeImage
+    if ResizeImage is None:
+      image = Image.open(pathToImage.name)
+      image = Image.fromarray(image)
+    else:
+      image = Image.fromarray(ResizeImage)
+    w = int(width.get())
+    h = int(height.get())
+
+    resize_img = image.resize((w, h))
+    img = ImageTk.PhotoImage(resize_img)
+    disp_img.config(image=img)
+    disp_img.image = img
+
 
 def ApplyFilter(mode):
     global img_o
     global pathToImage
     global SaveImage
-
+    global ResizeImage
     if mode==warm:
        img_o=cv2.imread(pathToImage.name)
-       #img_o = cv2.cvtColor(img_o, cv2.COLOR_BGR2GRAY )
        img_o=cv2.cvtColor(img_o,cv2.COLOR_BGR2RGB)
        img_o=warm(img_o)
        SaveImage=img_o
+       ResizeImage=img_o
        scaling=(int(img_o.shape[1]*0.5),int(img_o.shape[0]*0.5))
        img_o=cv2.resize(img_o,scaling,interpolation=cv2.INTER_AREA)
        img_o=Image.fromarray(img_o)
        img_o=ImageTk.PhotoImage(img_o)
        label2_img=tk.Label(image=img_o)
        label2_img.grid(row=0,column=0,sticky=tk.E + tk.W + tk.N + tk.S)
+    elif mode==grayFilter:
+       img_o=cv2.imread(pathToImage.name)
+       img_o = cv2.cvtColor(img_o, cv2.COLOR_BGR2GRAY )
+       SaveImage=img_o
+       ResizeImage=img_o
+       scaling=(int(img_o.shape[1]*0.5),int(img_o.shape[0]*0.5))
+       img_o=cv2.resize(img_o,scaling,interpolation=cv2.INTER_AREA)
+       img_o=Image.fromarray(img_o)
+       img_o=ImageTk.PhotoImage(img_o)
+       label2_img=tk.Label(image=img_o)
+       label2_img.grid(row=0,column=0,sticky=tk.E + tk.W + tk.N + tk.S)         
+    elif mode==rgbFilter:
+       img_o=cv2.imread(pathToImage.name)
+       img_o=rgbFilter(img_o)
+       SaveImage=img_o
+      #  img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+      #  img=Image.fromarray(img)
+      #  img=ImageTk.PhotoImage(img)
+       label2_img=tk.Label(image=img_o)
+       label2_img.grid(row=0,column=0,sticky=tk.E + tk.W + tk.N + tk.S)       
     elif mode==save:
-         pathToModifiedImage ='C:\\Users\\Ovidiu\\Desktop\\ProiectTM\\Images' 
-         cv2.imwrite(os.path.join(pathToModifiedImage , 'modified.png'), SaveImage)
-         easygui.msgbox("Imaginea modificata a fost salvata!", title="simple gui")
-
+      if SaveImage is None:
+        easygui.msgbox("You need to modify an upload photo in order to save an image!", title="Inform message")
+      else:    
+        name = filedialog.asksaveasfilename(title="Save as...", filetypes=(('JPEG', ('*.jpg', '*.jpeg', '*.jpe')), ('PNG', '*.png'), ('BMP', ('*.bmp', '*.jdib')), ('GIF', '*.gif')), defaultextension=".png")
+      if name:
+        cv2.imwrite(name, SaveImage)
+        easygui.msgbox("The modified image has been saved!", title="Inform message")
+      else:
+        easygui.msgbox("Wrong Path!", title="Inform message")
+        
 
 def ShowMenu():
-  GrayBtn = Button(FirstWindow, text = 'Gray', bg= 'yellow', 
+  global width,height,disp_img
+  WarmBtn = Button(FirstWindow, text = 'Warm', bg= 'yellow', 
                     compound = RIGHT , command = lambda:[ApplyFilter(warm)])
-  GrayBtn.place(relx = 1, x =-2, y = 2, anchor = NE)
+  WarmBtn.place(relx = 1, x =-2, y = 2, anchor = NE)
 
-  SaveBtn = Button(FirstWindow, text = 'Save', bg= 'blue', 
+  GrayBtn = Button(FirstWindow, text = 'GrayFilter', bg= 'gray', 
+                    compound = RIGHT , command = lambda:[ApplyFilter(grayFilter)])
+  GrayBtn.place(relx = 1, x =-2, y = 50, anchor = NE)
+
+  RGBBtn = Button(FirstWindow, text = 'RGB-Filter', bg= 'green', 
+                    compound = RIGHT , command = lambda:[ApplyFilter(rgbFilter)])
+  RGBBtn.place(relx = 1, x =-2, y = 98, anchor = NE)
+
+  SaveBtn = Button(FirstWindow, text = 'Save', bg= 'red', 
                     compound = RIGHT , command = lambda:[ApplyFilter(save)])
-  SaveBtn.place(relx = 1, x =-2, y = 50, anchor = NE)
+  SaveBtn.place(relx = 1, x =-2, y = 146, anchor = NE)
+
+  frame = Frame(FirstWindow)
+  frame.grid()
+
+  Label(
+      frame,
+      text='Width'
+      ).grid(column= 0)
+  width = Entry(frame, width=10)
+  width.insert(END, 300)
+  width.grid(column= 0)
+
+  Label(
+      frame,
+      text='Height'
+      ).grid(column=0)
+
+  height = Entry(frame, width=10)
+  height.insert(END, 350)
+  height.grid(column= 0)
+
+  disp_img = Label()
+  disp_img.grid(pady=1)
+
+  resize_btn = Button(
+      frame,
+      text='Resize',
+      command=resize_func
+  )
+  resize_btn.grid(column= 0)
+
 
 # Adding widgets to the root FirstWindow
 BrowseLabel = Label(FirstWindow, text = 'Click on the below button to pick a new image on the canvas\n - After you pick a photo this text will disappear\n - And a lot of buttons will apeer', font =(
